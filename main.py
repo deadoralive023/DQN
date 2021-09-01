ff441aded238a24f56091bc9bbfeb22fc2a2bd09
deadoralive023 <antelopes06@gmail.com>
2021-09-01 23:15:26 +0500

    Added Agent class, moved most of the code from main to agent

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Net(nn.Module):
     def __init__(self, num_actions):
         super().__init__()
         self.conv_layers = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=4, stride=2, padding=0),
+            nn.Conv2d(4, 16, kernel_size=4, stride=2, padding=0),
             nn.ReLU(),
             nn.Conv2d(16, 32, kernel_size=4, stride=4, padding=0),
             nn.ReLU(),
@@ -34,7 +34,7 @@ class Net(nn.Module):
         return output
 
 
-class Preprocess:
+class PreProcess:
     def __init__(self, stack_size=4):
         self.stack_size = stack_size
         self.prev_frames = deque(maxlen=stack_size)
@@ -49,7 +49,6 @@ class Preprocess:
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
         frame = frame / 255
-        frame = frame[np.newaxis, :, :]
         return frame
 
 class BufferReplay:
@@ -67,36 +66,69 @@ class BufferReplay:
     def discretized_sample(self, size):
         size = min(size, len(self.buffer))
         indexes = sample(range(0, len(self.buffer)), size)
-        states = torch.tensor([self.buffer[i][0] for i in indexes])
+        states = torch.Tensor([self.buffer[i][0] for i in indexes])
         actions = torch.tensor([self.buffer[i][1] for i in indexes])
-        rewards = torch.tensor([self.buffer[i][2] for i in indexes])
-        next_states = torch.tensor([self.buffer[i][3] for i in indexes])
+        rewards = torch.Tensor([self.buffer[i][2] for i in indexes])
+        next_states = torch.Tensor([self.buffer[i][3] for i in indexes])
         terminals = torch.tensor([self.buffer[i][4] for i in indexes])
         return [states, actions, rewards, next_states, terminals]
 
-def main():
+class Agent:
+    def __init__(self, env):
+        self.env = env
+        self.pp = PreProcess()
+        self.br = BufferReplay()
+        self.value_net = Net(env.action_space.n)
+        self.prev_obs = self.env.reset()
+        self.prev_obs = self.pp.pre_process(self.prev_obs)
+        self.prev_obs = self.pp.stack_frames(self.prev_obs)
+
+    def step(self, action):
+        obs, reward, done, _ = self.env.step(action)
+        obs = self.pp.pre_process(obs)
+        obs = self.pp.stack_frames(obs)
+        self.br.add([self.prev_obs, action, reward, obs, int(done)])
+        self.prev_obs = obs
+        if done:
+            self.prev_obs = self.env.reset()
+            self.prev_obs = self.pp.preprocess(self.prev_obs)
+            self.prev_obs = self.pp.stack_frames(self.prev_obs)
+
+        return obs, reward, done
+
+    def learn(self):
+        pass
+
+def main(render=False):
     env = gym.make('Breakout-v0')
-    obs = env.reset()
+    agent = Agent(env)
 
     while True:
-        env.render()
-        time.sleep(0.005)
         action = env.action_space.sample()
-        obs, reward, done, _ = env.step(action)
-        if done:
-            obs = env.reset()
+        obs, reward, done = agent.step(action)
+        ipdb.set_trace()
+
+        sample = agent.br.discretized_sample(10)
+        output = agent.value_net(sample[0])
+        
+        if render:
+            env.render()
+            time.sleep(0.005)
+
         if k.is_pressed('q'):
             break
-        index += 1
 
 
 
 if __name__ == '__main__':
-    #main()
+    main()
     #env = gym.make('Breakout-v0')
     #pp = Preprocess()
-    #obs = pp.pre_process(obs)
+    #obs = pp.pre_process(env.reset())
     #ipdb.set_trace()
+
+    stacks = pp.stack_frames(obs)
+
     #obs_t = torch.Tensor(obs)
     #obs_t = torch.cat([obs_t, obs_t]).unsqueeze(1)
     #net = Net(4)
